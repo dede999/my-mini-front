08e2f38dc28cad1eb1f515fd6575556efd0f4b4b
André Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2020-05-04 22:56:02 -0300

    Creating services

---
+++ src/service/headers_handler.ts
@@ -0,0 +1,25 @@
+
+export const save_headers = (response: any) => {
+  localStorage.setItem("accessToken", response["access-token"])
+  const { client, uid } = response
+  localStorage.setItem("uid", uid)
+  localStorage.setItem("client", client)
+}
+
+export const clear_headers = () => {
+  localStorage.removeItem("uid")
+  localStorage.removeItem("client")
+  localStorage.removeItem("accessToken")
+}
+
+export const get_headers = () => {
+  return {
+    uid: localStorage.getItem("uid"),
+    client: localStorage.getItem("client"),
+    "access-token": localStorage.getItem("accessToken")
+  }
+}
+
+export const set_access_token = (token: string) => {
+  localStorage.setItem("accessToken", token)
+}
