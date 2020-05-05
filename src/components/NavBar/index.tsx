2de4cf05031e7d039158e59e2b5671a28990171d
André Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2020-05-04 22:56:46 -0300

    NavBar component created

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,90 @@
+import React, {Component} from "react"
+import { TextInput } from "evergreen-ui"
+import {clear_headers, save_headers} from '../../service/headers_handler'
+import api from "../../service/api"
+import './style.sass'
+
+type AuthHeaders = {
+  uid: string | null,
+  client: string | null,
+  accessToken: string | null
+}
+
+interface Props {
+  email: string,
+  password: string,
+  headers: AuthHeaders
+}
+
+export default class NavBar extends Component<{}, Props> {
+  state = {
+    headers: {
+      uid: localStorage.getItem("uid"),
+      client: localStorage.getItem("client"),
+      accessToken: localStorage.getItem("accessToken"),
+    },
+    email: 'Email',
+    password: 'Password'
+  }
+
+  is_logged_in = (): boolean => {
+    return this.state.headers.uid !== null
+  }
+
+  logging_in = async () => {
+    await api.post("auth/sign_in", {
+        email: this.state.email,
+        password: this.state.password,
+    }).then(resp => {
+      console.log(resp.data)
+      save_headers(resp.headers)
+    })
+      .catch(err => console.error(err))
+      .finally(() => window.location.reload(false))
+  }
+
+  logging_out = async () => {
+    await api.delete("auth/sign_out", {
+      headers: {
+        uid: this.state.headers.uid,
+        client: this.state.headers.client,
+        "access-token": this.state.headers.accessToken
+      }
+    }).then(() => clear_headers())
+      .catch(err => console.error(err)).finally(() => window.location.reload(false))
+  }
+
+  render() {
+    return (
+      <div className='nav-bar'>
+        <h1> Mini App </h1>
+
+        {localStorage.getItem("uid")
+          ? <div id="client">
+            { this.state.headers.uid }
+            <button onClick={this.logging_out}>
+              Log Out
+            </button>
+          </div>
+          :
+          <div id="login">
+            <div id="fields">
+              <TextInput
+                placeholder={this.state.email}
+                width={225}
+                marginRight={10}
+                onChange={(e: any) => this.setState({ email: e.target.value })} />
+              <TextInput
+                placeholder={this.state.password}
+                width={225}
+                onChange={(e: any) => this.setState({ password: e.target.value })} />
+            </div>
+            <div id="login-btn">
+              <button onClick={this.logging_in}> Log In </button>
+            </div>
+          </div>
+        }
+      </div>
+    )
+  }
+}
